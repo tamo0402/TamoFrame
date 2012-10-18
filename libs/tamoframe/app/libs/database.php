92b5575b17c6961e2c6a5124e5db7fff42553b6a
tamo0402 <maximum.saboten@gmail.com>
2012-10-18 14:25:20 +0900

    daoファイル作成。
    Model使う時の処理をコメントアウトで表示。

--- libs/tamoframe/app/libs/database.php
+++ libs/tamoframe/app/libs/database.php
@@ -4,7 +4,7 @@ class database {
 
 	// DB接続に必要な情報を入れる。
     protected $serverName   = "localhost";
-    protected $serverId     = "root";
-    protected $serverPass   = "";
-    protected $databaseName = "member";
+    protected $serverId     = "id";
+    protected $serverPass   = "password";
+    protected $databaseName = "dbname";
 }
